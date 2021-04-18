3ba728e7024157cd5c40eda892ad0db2f1a2398c
Vishal Singh <VishalSingh1597>
2021-04-18 18:51:40 +0530

    Added Validate Pin Code from taking alphabets or special character

--- Program.cs
+++ Program.cs
@@ -11,23 +11,16 @@ namespace RegexDemo
     {
         static void Main(string[] args)
         {
-            String pattern = "^[0-9]*[a-zA-Z]{3,}[a-zA-Z0-9]*$";
-            string[] inputs = { "111", "1aaa", "aa11", "bcc", "1a1aab", "1abb23a" };
             string pincode = "^[0-9]{3}[]*[0-9]{3}$";
-            string[] pinCodeArr = { "422010", "412109", "400820", "1452" };
+            string[] pinCodeArr = { "422010", "A42109", "40C820", "145225" };
             Program p = new Program();
-            p.ValidateConseqCharacter(inputs, pattern);
-            Console.WriteLine();
+
             p.ValidPincode(pinCodeArr, pincode);
+            Console.WriteLine();
             Console.Read();
 
         }
-        public void ValidateConseqCharacter(string[] arr, string pattern)
-        {
-            Console.WriteLine("Validaing 3 Conseq Characters");
-            Regex regex = new Regex(pattern);
-            IterateLoop(arr, regex);
-        }
+
         public void ValidPincode(string[] arr, string pincode)
         {
             Console.WriteLine("Validation Indian PIN Code");
